ac30d27e00cf7687e48ccb4d7bbeca278db4cf79
BurdetteLamar <burdettelamar@yahoo.com>
2025-12-02 00:18:29 +0000

    [DOC] More on CGI#http_header

--- lib/cgi/core.rb
+++ lib/cgi/core.rb
@@ -354,7 +354,7 @@ class CGI
       buf << EOL    # empty line of separator
       return buf
     end
-  end
+  end # http_header()
 
   # This method is an alias for #http_header, when HTML5 tag maker is inactive.
   #
@@ -363,7 +363,6 @@ class CGI
   #
   # Using #header with the HTML5 tag maker will create a <header> element.
   alias :header :http_header
-  
   def _no_crlf_check(str)
     if str
       str = str.to_s
